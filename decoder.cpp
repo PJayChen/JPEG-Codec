9c507bf9e7ebf51644b8f99fb1c3a3db65d2407f
PJayChen <n26021539@mail.ncku.edu.tw>
2014-06-12 01:25:01 -0700

    add decode DC function

--- decoder.cpp
+++ decoder.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 typedef unsigned char  Byte;
 
@@ -8,6 +9,8 @@ class JPEGimage{
 
 public:
 	void loadJPGEimage(const char *fileName);
+	int decodeDC(void);
+
 	JPEGimage(){
 		JpegSizeX = 0;
 		JpegSizeY = 0;
@@ -19,20 +22,64 @@ public:
 
 private:
 	char *bitstream_data;
+	char *bdptr; //point to the current processing position of bitstream_data
 	int JpegSizeX, JpegSizeY;
-    int decodeDC(int *position);
+    
 };
+
+
 /*
-int JPEGimage::decodeDC(int *position)
-{
-    int codeLen[12] = {2,4,5,6,7,8,10,12,14,16,18,20};
-    const char* code[12] = {"00","010","011","100","101","110","1110","11110","111110","1111110","11111110","111111110"};
-    int i = 0, lenth_case = 0;
-
-    if(strncmp(bitstream_data, code[0], 2) == 0);
-
-}
+* DC is composition with {SIZE, VALUE}
+* first we need to find out the SIZE part
+* than we can know the length of VALUE
+* so the next step is convert the VALUE from binary string(char *) to decimal(int)
+*
+* note: use the "bdptr" to point the currnet processing position of "bitstream_data"
 */
+int JPEGimage::decodeDC(void)
+{
+    const int codeTotalLen[12] = {2,4,5,6,7,8,10,12,14,16,18,20};
+    const int codeLen[12] = {2, 3, 3, 3, 3, 3, 4, 5, 6, 7, 8, 9};
+    const char* code[12] = {"00","010","011","100","101","110","1110","11110","111110","1111110","11111110","111111110"};
+    int i = 0;
+    
+    //Find the which DC code is
+    for(i = 0; i < 12 ; i++)
+    	if(strncmp(bdptr, code[i], codeLen[i]) == 0)
+    		break;
+    
+    //move pointer to point to "VALUE" of DC
+    bdptr += codeLen[i];
+    //find the length of VALUE 
+    int valueLen = codeTotalLen[i] - codeLen[i];
+    
+    //store the VALUE part of DC in value
+    char *value;
+    value = (char *) malloc(valueLen * sizeof(char) + 1);
+    value = strncpy(value, bdptr, valueLen);
+    value[valueLen] = '\0';
+
+    //move ptr to next component of the bitstream
+    bdptr += valueLen;
+
+    int DC = 0;
+    //convert binary str to integer
+    for(int j = valueLen-1; j >= 0  ; j--){
+    	if(value[j] == '1')
+    		DC += (int)pow(2,valueLen - (j+1));
+    }
+    //get the 1's complementation
+    if(value[0] == '0'){
+    	DC = DC + 1 - (int)pow(2, valueLen);
+    }
+
+    //printf("bitstream_data start from %d, current is %d\n", bitstream_data, bdptr);
+    printf("DC: %s(%d)  \n", value, DC);
+    
+
+    return DC;
+}
+
 void JPEGimage::loadJPGEimage(const char *fileName)
 {
 	long file_size = 0;
@@ -60,7 +107,7 @@ void JPEGimage::loadJPGEimage(const char *fileName)
 
 	//allocate memory for the string to contain read bitstream
 	bitstream_data = (char *) malloc((file_size * 8 * sizeof(char)) + 1);
-    
+    bdptr = bitstream_data;
 	//translate the binary to store in string(ex: 0xf0 -> "11110000")
 	while(!feof(fp)){
 		read_byte = getc(fp);
@@ -75,6 +122,8 @@ void JPEGimage::loadJPGEimage(const char *fileName)
 	bitstream_data[file_size*8] = '\0'; 
 	printf("The file content is \n%s\n", bitstream_data);
 	if(fp) fclose(fp);
+
+	decodeDC();
 }
 
 int main(void)
@@ -82,5 +131,6 @@ int main(void)
 	JPEGimage jj;
 
 	jj.loadJPGEimage("out.Ajpg");
+	//jj.decodeDC(0);
 	return 0;
 }
